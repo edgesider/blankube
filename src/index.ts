fd2b3af734aa12eb61f39dbb51393ad0539bc3d1
dykai <yingkaidang@gmail.com>
2020-12-20 12:06:41 +0800

    添加自适应画布大小

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,19 @@ import {SCENE_COLOR} from "./constants";
 const canvas: HTMLCanvasElement = document.querySelector('#game')
 export const scene = new Three.Scene()
 export const camera = new Three.PerspectiveCamera(
-    50, canvas.clientWidth / canvas.clientHeight, 0.1, 1000)
+    50, window.innerWidth / window.innerHeight, 0.1, 1000)
 
 const renderer = new Three.WebGLRenderer({canvas})
 renderer.setSize(window.innerWidth, window.innerHeight)
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+})
 
 scene.background = new Three.Color(SCENE_COLOR)
-export const cube = new RubikCube(scene, 3, 6)
-camera.position.set(cube.cubeSize * 1.3, cube.cubeSize * 1.3, cube.cubeSize * 2)
+export const cube = new RubikCube(scene, 3, 200)
+camera.position.set(cube.cubeSize * 1.3, cube.cubeSize * 1.3, cube.cubeSize * 3)
 camera.lookAt(0, 0, 0)
 
 listenKey(cube)
