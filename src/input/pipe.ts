dd2a49377197b64c2da4743f437b388df5a9b861
dangyingkai <dangyingkai@bytedance.com>
2021-08-14 19:20:03 +0800

    开启另一个分支，用于设计和思考协程事件处理

--- src/input/pipe.ts
+++ src/input/pipe.ts
@@ -1,3 +1,26 @@
+/**
+ * 过程式的协程与函数式的事件流并不冲突。协程适合多个事件和过程的组合等待的场景，函数式的方式适合单个事件的处理。
+ *
+ * 常规的函数式的数据流方式并未提供将过程串在一起的方式，因此并未摆脱回调；它主要是利用了函数式强大地表达能力，简化了
+ * 对事件的处理。
+ *
+ * 在使用协程时，仍旧可以使用函数式的方式对事件作预处理（map、filter等），但是事件并不以
+ * 订阅的方式消耗，而是在协程中进行消耗，参与进整个过程的组合拼接中。
+ *
+ *
+ * 协程为开发者提供了无限多个独立的"过程容器"，我们不必去思考怎样将很多个过程安排到一个线程中，而是去如何将现有的事件
+ * 组织成一个完整的过程；不必站在计算资源的角度来思考，而是站在任务的角度思考。
+ *
+ * 与所有的事件驱动一样，整个协程框架可以说是被众多事件推着前进的，这些事件就像齿轮一样，互相咬合、互相推进；因此协程
+ * 框架应当支持高效地事件传递和转化。
+ *
+ * 协程框架应当支持协程的中断。
+ *
+ * 结构化协程是一种数据组织和协程生命周期管理方式。
+ *
+ * 协程不是银弹，但它一定是另一种模式。
+ */
+
 export abstract class Closable {
     protected _closed = false
 
@@ -17,7 +40,8 @@ export abstract class Closable {
         this.afterClose(first)
     }
 
-    protected afterClose(firstClose: boolean) {}
+    protected afterClose(firstClose: boolean) {
+    }
 }
 
 export interface ISource<T> {
@@ -163,3 +187,23 @@ export class FuncSink<T> implements ISink<T> {
     }
 }
 
+const CanceledException = {}
+
+function CPromise(e) {
+    let reject = null
+    const p = new Promise((res, rej) => {
+        reject = rej
+        e(res, rej)
+    })
+    p['cancel'] = () => reject(CanceledException)
+    return p
+}
+
+function CancelablePromise<T>(executor: (
+    resolve: (value: T | PromiseLike<T>) => void,
+    reject: (reason?: any) => void) => void) {
+}
+
+const p = new Promise((res, rej) => {
+
+})
