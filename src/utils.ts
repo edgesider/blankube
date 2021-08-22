c70c511780a75b53a5b16ac6cdfa3632e7712bdf
dangyingkai <dangyingkai@bytedance.com>
2021-08-22 12:56:33 +0800

    更新README；可自定义坐标轴的长度

--- src/utils.ts
+++ src/utils.ts
@@ -26,13 +26,13 @@ export async function delay(ms: number): Promise<void> {
     })
 }
 
-export function addAxis(obj) {
+export function addAxis(obj, length = 10) {
     const geoX = new Three.BufferGeometry().setFromPoints([
-            new Three.Vector3(0, 0, 0), new Three.Vector3(5, 0, 0)]),
+            new Three.Vector3(0, 0, 0), new Three.Vector3(length, 0, 0)]),
         geoY = new Three.BufferGeometry().setFromPoints([
-            new Three.Vector3(0, 0, 0), new Three.Vector3(0, 5, 0)]),
+            new Three.Vector3(0, 0, 0), new Three.Vector3(0, length, 0)]),
         geoZ = new Three.BufferGeometry().setFromPoints([
-            new Three.Vector3(0, 0, 0), new Three.Vector3(0, 0, 5)])
+            new Three.Vector3(0, 0, 0), new Three.Vector3(0, 0, length)])
     const mtrX = new Three.LineBasicMaterial({color: 0xFF0000, linewidth: 10}),
         mtrY = new Three.LineBasicMaterial({color: 0xFF00, linewidth: 10}),
         mtrZ = new Three.LineBasicMaterial({color: 0xFF, linewidth: 10})
@@ -41,16 +41,16 @@ export function addAxis(obj) {
     const geoCor = new Three.ConeGeometry(.3, 1)
     const mtrCorX = new Three.MeshBasicMaterial({color: 0xFF0000})
     const corX = new Three.Mesh(geoCor, mtrCorX)
-    corX.position.set(5, 0, 0)
+    corX.position.set(length, 0, 0)
     corX.rotateOnAxis(new Three.Vector3(0, 0, 1), -Math.PI / 2)
 
     const mtrCorY = new Three.MeshBasicMaterial({color: 0xFF00})
     const corYObj = new Three.Mesh(geoCor, mtrCorY)
-    corYObj.position.set(0, 5, 0)
+    corYObj.position.set(0, length, 0)
 
     const mtrCorZ = new Three.MeshBasicMaterial({color: 0xFF})
     const corZObj = new Three.Mesh(geoCor, mtrCorZ)
-    corZObj.position.set(0, 0, 5)
+    corZObj.position.set(0, 0, length)
     corZObj.rotateOnAxis(new Three.Vector3(1, 0, 0), Math.PI / 2)
     obj.add(corX, corYObj, corZObj)
 }
